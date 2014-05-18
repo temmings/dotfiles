397a880d7073b42190710aefc3b1c8d9a735b4df
Tetsuya HAMAMOTO <temmings@gmail.com>
2014-05-18 12:49:23 +0900

    add Vim configurations

---
+++ vim/install.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+local install_script_dir=$(dirname $0(:A))
+local vim_dir=$HOME/.vim
+local -a dotfiles
+dotfiles=(vimrc gvimrc)
+vim_dir_files=(vimrc.shadow autoload doc ftplugin indent plugin spell syntax templates)
+
+mkdir -p $vim_dir
+mkdir -p $vim_dir/backup
+mkdir -p $vim_dir/bundle
+mkdir -p $vim_dir/undo
+
+for f in $dotfiles; do
+    ln -sv $install_script_dir/$f ~/.$f
+done
+for f in $vim_dir_files; do
+    ln -sv $install_script_dir/$f $vim_dir/$f
+done
