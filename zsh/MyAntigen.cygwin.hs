ae4062966793c92196807bfad161d52fb2a2913b
Tetsuya HAMAMOTO <t.hamamoto@suvt.co.jp>
2015-02-24 12:48:47 +0900

    sync

---
+++ zsh/MyAntigen.cygwin.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+module MyAntigen where
+
+import Antigen (
+                -- Rudimentary imports
+                AntigenConfiguration (..)
+              , bundle
+              , antigen
+                -- If you want to source a bit trickier plugins
+              , ZshPlugin (..)
+              , antigenSourcingStrategy
+              , filePathsSourcingStrategy
+              )
+import Shelly (shelly)
+
+bundles =
+  [ bundle "temmings/shell-todo"
+  , (bundle "rupa/z") { sourcingStrategy = filePathsSourcingStrategy [ "z.sh" ] }
+  , (bundle "m4i/cdd") { sourcingStrategy = filePathsSourcingStrategy [ "cdd" ] }
+  , (bundle "robbyrussell/oh-my-zsh")
+     { sourcingStrategy = filePathsSourcingStrategy [ "oh-my-zsh.sh" ] }
+  ]
+
+config = AntigenConfiguration bundles
+
+main :: IO ()
+main = shelly $ antigen config
